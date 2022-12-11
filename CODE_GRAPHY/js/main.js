59a435b7830e7c31f557fdf2d24e1b0db76ed8b9
김규민 <57985935+kgm4082@users.noreply.github.com>
2022-12-12 08:37:10 +0900

    Update

---
+++ CODE_GRAPHY/js/main.js
@@ -0,0 +1,12 @@
+// 커피베이 포트폴리오 메인 JS - main.js
+
+$(() => {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 5,
+        spaceBetween: 20,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+}); //////////////////// jQB ////////////////////
