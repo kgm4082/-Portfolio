1b1fc4f9018239a8d7bdd30917a3c69fc952349d
김규민 <57985935+kgm4082@users.noreply.github.com>
2023-03-06 00:50:27 +0900

    Update App.js

--- props/src/App.js
+++ props/src/App.js
@@ -1,12 +1,36 @@
 import Main from './component/main/main';
 import Loading from './component/loading/loading';
-import './App.css';
+import { useEffect } from 'react';
+
 function App() {
+  useEffect(() => {
+    Main();
+  },[]);
   return (
     <>
-      <Loading />
-      <div id='info'>1111111111</div>
-      <Main/>
+      {/* <Loading /> */}
+      <div id="container">
+        <canvas id="c"></canvas>
+        <div id="labels"></div>
+        <div className='header'>
+          <h2>WORLD MAP <br />CYBER MISSIONS </h2>
+          <p>SELECT A MISSION TO BEGIN</p>
+          <h2>USA</h2>
+        </div>
+        <div className='select'>
+          <h4>SELECT A REGION</h4>
+        </div>
+        <div className='interface'></div>
+        <div className='chat'></div>
+        <div className='footer'>
+          <div className='logo'>
+            {/* <img src=''/> */}
+          </div>
+          <div className='info'>
+              
+          </div>
+        </div>
+      </div>
     </>
   );
 }
